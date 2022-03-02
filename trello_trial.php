51330278b9fda96819b0a3bc4580e930401095cb
kacperkinasiewicz <kacper.kinasiewicz.1@gmail.com>
2022-03-02 13:40:35 +0100

    PHP 3

--- trello_trial.php
+++ trello_trial.php
@@ -1,27 +1,64 @@
 <?php
 
-$serwer = "localhost";
-$user = "root";
-$pass = "";
-$base = "trello_trial";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "trello_trial";
 
-$db = @new mysqli($serwer, $user, $pass, $base);
-
-if ($db->connect_error) 
+try 
 {
-    die("<br>Błąd połączenia z bazą danych: ". $db->connect_error. ". <br>");
+    $dbh = new PDO('mysql:host=localhost;dbname=trello_trial', $username, $password);
+    echo "Connected\n";
+} 
+catch (Exception $e) 
+{
+    die("Unable to connect: " . $e->getMessage());
 }
-
-else
-{
-    echo "<br>Nawiązano połączenie z bazą danych: ". $base. ". <br>";
-    $login = !empty($_POST['login']) ? htmlspecialchars(stripslashes(trim($_POST['login']))) : false;
-    $haslo = !empty($_POST['haslo']) ? htmlspecialchars(stripslashes(trim($_POST['haslo']))) : false;
-    $powtorz = !empty($_POST['powtorz']) ? htmlspecialchars(stripslashes(trim($_POST['powtorz']))) : false;
+  
+try 
+{  
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+    $dbh->beginTransaction();
+    $name = !empty($_POST['Task']) ? htmlspecialchars(stripslashes(trim($_POST['name']))) : false;
+    $picture = !empty($_POST['Picture']) ? htmlspecialchars(stripslashes(trim($_POST['picture']))) : false;
+    $description = !empty($_POST['Description']) ? htmlspecialchars(stripslashes(trim($_POST['description']))) : false;
+    $dbh->exec("insert into tasks (Name, Picture, Description) values ($name, $picture, $description)");
+    $dbh->commit();
     
+} 
+catch (Exception $e) 
+{
+    $dbh->rollBack();
+    echo "Failed: " . $e->getMessage();
 }
-
-
-$db->close();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Trello_trial</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Trello_trial</h2>
+<form method="post" id="contact-form">
+
+    <div><label for="name">Task: </label></div>
+    <div><input type="text" name="Task" id="Task" class="formField"></div>
+
+    <div><label for="email">Picture: </label></div>
+    <div><input type="text" name="Picture" id="Picture" class="formField"></div>
+
+    <div><label for="phone">Description: </label></div>
+    <div><input type="text" name="Description" id="Description" class="formField"></div>
+
+    <div><button id="sendBtn" name="sendBtn">Add</button></div>
+
+</form>
+
+</body>
+</html>
