1b997a39a88ab280473ab23ac38fdc6b44f53089
kacperkinasiewicz <kacper.kinasiewicz.1@gmail.com>
2022-03-03 11:46:46 +0100

    PHP 44, CSS4

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Trello_trial</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 <div class="list all">
-<div class="header all">Tasks:</div>
-<div class="task all">Task1</div>
-<div class="task all">Task2wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</div>
-<div class="all addbutton"><a href="addtask.php">Add task</a></div>
-<div class="task all">Task3</div>
-
+    <div class="header all">Tasks:</div>
+    <div class="task all">Task1</div>
+    <div class="task all">Task2wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</div>
+    <div class="task all">Task3</div> 
+    <div class="all" id="addbutton"><a href="addtask.php">Add task</a></div>
 </div>   
-
-<?php echo 'test'; ?>
-
 </body>
+
 </html>
