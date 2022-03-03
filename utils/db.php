912897a95dc9856510172d33543df858b5e9387f
mati31203 <mati31203@onet.pl>
2022-03-03 12:03:35 +0100

    add startConnection and getAll functions in utils

--- utils/db.php
+++ utils/db.php
@@ -1,14 +1,28 @@
 <?php
 
-function connection()
+function startConnection(): ?PDO
 {
-    $db_conn = new PDO("mysql:host=localhost;dbname=trello_trial", "root", "");
-
-    try{
+    try
+    {
         $db_conn= new PDO("mysql:host=localhost;dbname=trello_trial", "root", "");
-    } catch (PDOException $e){
-        echo "Błąd połączenia z bazą danych";
+        $db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e)
+    {
+        return null;
     }
     return $db_conn;
 }
-connection();
+
+function getAll(): bool|array
+{
+    $db_conn = startConnection();
+    if (!is_null($db_conn)) {
+        $stmt = $db_conn->prepare('SELECT * FROM tasks');
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    return [];
+}
