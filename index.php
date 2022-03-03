3f5facb9de4532eaeedb033c594b773df6662ee7
kacperkinasiewicz <kacper.kinasiewicz.1@gmail.com>
2022-03-03 10:15:49 +0100

    PHP 3, CSS3, Addtask 1

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Trello_trial</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class="list all">
+<div class="header all">Tasks:</div>
+<div class="task all">Task1</div>
+<div class="task all">Task2wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</div>
+<div class="all addbutton"><a href="addtask.php">Add task</a></div>
+<div class="task all">Task3</div>
+
+</div>   
 
 </body>
 </html>
