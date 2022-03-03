558605401eae2366c6110275856a127edd09bd57
kacperkinasiewicz <kacper.kinasiewicz.1@gmail.com>
2022-03-03 14:31:03 +0100

    PHP5

---
+++ addtask.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Trello_trial</title>
+    <link rel="stylesheet" href="addtask_style.css">
+</head>
+
+<body>
+    <div id="form">
+        <form action="" method="POST" enctype="multipart/form-data">
+            <div class="input" id="name">Name: <br><textarea name="name" id="textname"></textarea></div>
+            <div class="input" id="picture">Picture: <br><input type="file" name="picture"></div>
+            <div class="input" id="description">Description: <br><textarea name="description" id="textdesc"></textarea></div>
+            <div id="button"><input type="submit" name="add" value="Add task"></div>
+        </form>
+    </div>
+</body>
+</html>
+
