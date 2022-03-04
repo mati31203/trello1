3d04b3dc5c83503561b2fd1b0fae3de2e92b46e1
mati31203 <mati31203@onet.pl>
2022-03-04 08:16:49 +0100

    included create from db.php

--- addtask.php
+++ addtask.php
@@ -1,3 +1,12 @@
+<?php
+include_once "utils\db.php";
+while (!empty($_POST["add"]));
+{
+    $tasks=create();
+    break;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 
@@ -12,10 +21,9 @@
         <form action="" method="POST" enctype="multipart/form-data">
             <div class="input" id="name">Name: <br><textarea name="name" id="textname"></textarea></div>
             <div class="input" id="picture">Picture: <br><input type="file" name="picture"></div>
-            <div class="input" id="description">Description: <br><textarea name="description" id="textdesc"></textarea></div>
+            <div class="input" id="description">Description: <br><label for="textdesc"></label><textarea name="description" id="textdesc"></textarea></div>
             <div id="button"><input type="submit" name="add" value="Add task"></div>
         </form>
     </div>
 </body>
 </html>
-
