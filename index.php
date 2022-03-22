6b3e5981cc63ea4664ba1ae4e4ae839114dd893d
kacperkinasiewicz <kacper.kinasiewicz.1@gmail.com>
2022-03-22 13:12:31 +0100

    updates 2 22.03.2022

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ $lists = getList();
 <head>
     <meta charset="UTF-8">
     <title>Trello_trial</title>
-
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
     <script src="//code.jquery.com/jquery-1.12.4.js"></script>
     <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -19,47 +18,74 @@ $lists = getList();
 </head>
 
 <body>
-
 <div class="container">
+    <header> <img src="Group1.png"> </header>
+
+    <div class="lists">
+
+        <?php foreach($lists as $list):
+            $id_l = $list['id_l'];
+            $tasks = getAll($id_l); ?>
+            <div class="onelist">
+
+                <div class="listname" data-id="<?=$list['id_l'];?>">
+                    <?=$list['name'];?>
+                </div>
+
+                <div class="sortable connectedSortable">
+                    <?php foreach($tasks as $task):?>
+                            <div class="ui-state-default task">
+
+                                <div data-id="<?=$task['id'];?>">
+                                    <div><?=$task['name'];?></div>
+                                    <div><a class="detailsbuttons" href="details.php?id=<?=$task['id']; ?>">Details</a></div>
+                                </div>
+                            </div>
+                    <?php endforeach ?>
+                </div>
+
+                <a href="addtask1.php?id_l=<?=$id_l; ?>">Add task</a>
+
+            </div>
+
+        <?php endforeach ?>
+
+    </div>
+
+    <footer>
+        <div class="reordertasksbutton">
+            <form action="reorder.php" method="POST">
+                <input type="hidden" name="tasks_order">
+                <input type="submit" name="reorder" value="Zapisz kolejność" ><img src="saveIcon.png">
+            </form>
+        </div>
+
+        <a class="addnewlistbutton" href="addlist.php">Dodaj listę <img src="addIcon.png"></a>
+    </footer>
+
+
+
+
+
+
+</div>
+
+
+
 
 <!--
-    <?php foreach($lists as $list):
-        $id_l = $list['id_l'];
-        $tasks = getAll($id_l); ?>
-        <div class="all list" data-id="<?=$list['id_l'];?>">
-            <div class="all header">
-                <?=$list['name'];?>
-            </div>
-            <div class="sortable connectedSortable">
-                <?php foreach($tasks as $task):?>
-                    <div class="ui-state-default">
-                        <div class="all cell" data-id="<?=$task['id'];?>">
-                            <div class="all task"><?=$task['name'];?></div>
-                            <div class="all" id="detailsbutton"><a href="details.php?id=<?=$task['id']; ?>">Details</a></div>
-                        </div>
-                    </div>
-                <?php endforeach ?>
-            </div>
+
             <div class="all" id="addbutton">
                 <a href="addtask1.php?id_l=<?=$id_l; ?>">Add task</a>
             </div>
             <div class="all" id="addbutton"><a href="deletelist.php?id_l=<?=$id_l; ?>">Delete list</a></div>
         </div>
-    <?php endforeach ?>
-
-    <div>
-        <form action="reorder.php" method="POST">
-            <input type="hidden" name="tasks_order">
-            <input type="submit" name="reorder" value="Save order">
-        </form>
-    </div>
-
-    <div class="all" id="detailsbutton"><a href="addlist.php">Add New List</a></div>
-
-
 -->
-csadsadasddas
-</div>
+
+
+
+
+
     <script>
         $(document).ready(function ()
         {
@@ -91,9 +117,135 @@ csadsadasddas
             })
         });
     </script>
-
-
 </body>
 </html>
 
+<!--
+.all
+{
+    background: lightskyblue;
+    border: 5px ridge lightgrey;
+    border-radius: 7px;
+    font: 18px 'Trebuchet MS', sans-serif;
+    color:black;
+    word-wrap: break-word;
+    margin: 4px;
+    padding: 4px;
+}
 
+a
+{
+    text-decoration: none;
+    color: black;
+}
+
+.ui-state-default
+{
+    border: none;
+}
+
+.sortable
+{
+    min-height: 30px;
+}
+
+.ui-state-highlight
+{
+    background: turquoise;
+    height: 30px;
+    line-height: 20px;
+}
+
+.list
+{
+    background-color: lightskyblue;
+    width: 500px;
+    max-height: 800px;
+    overflow: auto;
+}
+
+.header
+{
+    width: 473px;
+    border-style: none;
+    font-weight: bold;
+    align-items: center;
+}
+
+.cell
+{
+    border-radius: 0px;
+    border-style: none;
+    margin: 0px;
+    padding: 0px;
+}
+
+.task
+{
+    background-color: white;
+    width: 395px;
+    min-height: 25px;
+    display: inline-block;
+}
+
+.task:hover
+{
+    background-color: lightgray;
+}
+
+#addbutton
+{
+    background-color: white;
+    height: 25px;
+    margin-right: 22px;
+    float: right;
+    vertical-align: bottom;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: black;
+}
+
+#addbutton:hover
+{
+    background-color: lightsteelblue;
+}
+
+#detailsbutton {
+    background-color: white;
+    height: 25px;
+    width: 50px;
+    font: 18px 'Trebuchet MS', sans-serif;
+    float: right;
+    vertical-align: bottom;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#detailsbutton:hover
+{
+    background-color: lightsteelblue;
+}
+
+input[type=submit]
+{
+    background-color: white;
+    height: 43px;
+    border: 5px ridge lightgrey;
+    border-radius: 7px;
+    font: 18px 'Trebuchet MS', sans-serif;
+    margin: 4px;
+    padding: 4px;
+    float: right;
+    vertical-align: bottom;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+input[type=submit]:hover
+{
+    background-color: lightsteelblue;
+    cursor: pointer;
+}
